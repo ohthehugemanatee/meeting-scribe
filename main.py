469e648611cdcdcec1a5a46c35dc938ddafc4c52
Campbell Vertesi <campbell@vertesi.com>
2018-04-24 16:24:29 -0700

    Create PUT method

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+# coding=utf-8
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+@app.route('/', methods=['POST'])
+def process_text():
+    if not request.json or not 'transcript' in request.json:
+        abort(400)
+    # Here we can do some processing on the text.
+    # For now, return a static response.
+
+    response = {
+        'summary': 'It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.',
+        'keywords': 'rebel, empire'
+    }
+    return jsonify(response), 200
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
