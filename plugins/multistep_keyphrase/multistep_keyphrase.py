a0a2b106bf905bf38ed49ecbae836f2df64f5d26
Campbell Vertesi <campbell@vertesi.com>
2018-04-28 18:29:27 -0700

    [WIP] Partial multistep keyword breakdown

--- plugins/multistep_keyphrase/multistep_keyphrase.py
+++ plugins/multistep_keyphrase/multistep_keyphrase.py
@@ -1,15 +1,27 @@
 from yapsy.IPlugin import IPlugin
+import re
+import nltk
+from nltk.corpus import stopwords
 
 class multistep_keyphrase(IPlugin):
     def process(self, text):
-        textByTopic = self.breakIntoTopics(text)
-        topicPhrases = {}
-        for topic, topicText in textByTopic.items():
-            topicPhrases[topic] = self.getKeyPhrases(topicText)
+        text = self.cleanText(text)
+        topicPhrases[topic] = self.getKeyPhrases(text)
 
         return {
                 'topicPhrases': topicPhrases
                 }
+
+    # Clean up punctuation etc.
+    def cleanText(self, text):
+        stop = stopwords.words('english')
+        document = ‘ ‘.join([i for i in document.split() if i not in stop])
+        sentences = nltk.sent_tokenize(document)V
+
+        text = re.sub('[^A-Za-z .-]+', ' ', text)
+        text = ' '.join(text.split())
+        text = ' '.join([i for i in text.split() if i not in stop])
+        return text
 
     # Break the transcript into an array of topic groupings.
     def breakIntoTopics(self, text):
@@ -21,7 +33,15 @@ class multistep_keyphrase(IPlugin):
 
     # Return keyphrases for a given chunk of text.
     def getKeyPhrases(self, text):
-       return [
+        # Get most frequently used nouns.
+        words = nltk.tokenize.word_tokenize(text)
+        words = [word.lower() for word in words if word not in stop]
+        fdist = nltk.FreqDist(words)
+        most_freq_nouns = [w for w, c in fdist.most_common(10)
+                                   if nltk.pos_tag([w])[0][1] in NOUNS]
+        # Extract entities.
+
+        return [
                'A keyphrase describes the core of what was discussed',
                'another keyphrase elaborates on the core of what was discussed'
                ]
